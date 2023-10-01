d4fb0ab6facb7af00126ba205fbfce68cc5ffdb8
ajayspatil7 <aspatil99451@gmail.com>
2023-10-01 12:53:22 +0530

    Can now fetch different terrains, coordinates.

--- datasets/api_sessions.py
+++ datasets/api_sessions.py
@@ -1,8 +1,23 @@
-import os
-import random
-
+import requests
 API_KEY = "SIGNATURE-HIDDEN"
 SESSION_TOKEN = "SIGNATURE-HIDDEN"
 
+# 1. URL infos
 
+params = {
+    "mapType": "satellite",
+    "language": "en-US",
+    "region": "US",
+}
 
+api_Endpoint = f"https://tile.googleapis.com/v1/createSession?key={API_KEY}"
+api_Header = {"content-type": "application/json"}
+
+resp = requests.post(api_Endpoint, headers=api_Header, json=params)
+
+if resp.status_code == 200:
+    data = resp.json()
+    print(data)
+    print(data["session"])
+else:
+    print(f"{resp.status_code}")
--- datasets/grabber.py
+++ datasets/grabber.py
@@ -1,27 +1,27 @@
-import os
-import time
 import api_sessions
 import requests
-import os
 
 # 1. URL infos
 mapVer = "v1"
 dim = "2dtiles"
 zoom = 15
-x = 6294
-y = 13288
+cordinates = [2500, 2500] # [lat, lng]
+rocky_dataset_folder = "datasets/rocky/"
+baseURL = f"https://tile.googleapis.com/{mapVer}/{dim}/{zoom}/{cordinates[0]}/{cordinates[1]}"
 
-baseURL = f"https://tile.googleapis.com/{mapVer}/{dim}/{zoom}/{x}/{y}"
-
+# 2. Querys
 querys = {
     "key": api_sessions.API_KEY,
     "session_token": api_sessions.SESSION_TOKEN,
 }
 
+# 3. Request
 response = requests.get(baseURL, params=querys)
 if response.status_code == 200:
-    with open("./ds/test_img.png", 'wb') as wr:
+    with open(f"{rocky_dataset_folder}colarado_img3.png", 'wb') as wr:
         wr.write(response.content)
+        print("Done")
 else:
-    f"{response.status_code}"
+    print(f"{response.status_code}")
+
 
